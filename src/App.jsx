6b670e58b62ffccddab16b00e94d44ddc6167a45
Mario <mario_bianchi_jr@hotmail.com>
2026-01-03 20:38:34 +0100

    ContactUs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from "./components/Nav";
+import ContactUs from "./components/ContactUs";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <Nav />
-    </>
+      <ContactUs />
+    </div>
   );
 }
 export default App;
