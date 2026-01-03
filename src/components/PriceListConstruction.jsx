7ee49d0b29b274d275373de13a7a0954188ff4ac
Mario <mario_bianchi_jr@hotmail.com>
2026-01-03 18:03:09 +0100

    Import of package

---
+++ src/components/PriceListConstruction.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriceListConstruction = () => {
+  return (
+    <div>PriceListConstruction</div>
+  )
+}
+
+export default PriceListConstruction
---
+++ src/components/Recaptcha.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Recaptcha = () => {
+  return (
+    <div>Recaptcha</div>
+  )
+}
+
+export default Recaptcha
