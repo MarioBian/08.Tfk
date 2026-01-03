0a3987ec994f0d792e4d2e21183c507cd86c546f
Mario <mario_bianchi_jr@hotmail.com>
2026-01-03 20:13:39 +0100

    started navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Nav />
+    </>
+  );
 }
 export default App;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="flex items-center justify-between bg-base-300 px-4 py-2 cursor-pointer">
+      <div className="flex items-center gap-4">
+        <a className="hover:underline">Hem</a>
+        <a className="hover:underline">Om oss</a>
+        <a className="hover:underline">Kontakt</a>
+        <a className="hover:underline">Prislista</a>
+        <a className="hover:underline">Offerter</a>
+      </div>
+
+      <div className="mr-5">
+        <input
+          type="text"
+          placeholder="Search"
+          className="input input-bordered input-sm"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
