5530ba158fc08c4ac0b0624dbd1c818a31a91672
Mario <mario_bianchi_jr@hotmail.com>
2026-01-03 19:49:18 +0100

    Installed tailwind and daisyui packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  return (
-    <>
-    </>
-  )
+  return <></>;
 }
-export default App
+export default App;
