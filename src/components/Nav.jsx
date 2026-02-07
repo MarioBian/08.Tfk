44b9839fb684869ac0aa6e0b266f8c338b761ad7
Mario <mario_bianchi_jr@hotmail.com>
2026-02-07 13:04:39 +0100

    Changed layout on navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="flex items-center justify-between bg-stone-600 px-4 py-2 cursor-pointer">
+    <div className="flex items-center justify-between bg-green-600 px-4 py-2 cursor-pointer">
       <div className="flex items-center gap-4">
         <a className="hover:underline">Hem</a>
         <a className="hover:underline">Om oss</a>
