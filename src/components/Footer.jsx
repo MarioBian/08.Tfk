c1ae803e4ab71dbed7dddcd42e1a6e21f8cc77ad
Mario <mario_bianchi_jr@hotmail.com>
2026-06-02 11:38:57 +0200

    moved footer more to the bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="mt-50">
       <p>
         &copy; 2026 <strong>Trädfällarkompissarna</strong>
       </p>
