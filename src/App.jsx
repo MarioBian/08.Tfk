4b57a3aaab31ca6a932ef7ef567cafa95e1db0b2
Mario <mario_bianchi_jr@hotmail.com>
2026-06-24 17:51:57 +0200

    deployed routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import "./App.css";
 import Nav from "./components/Nav";
 import ContactUs from "./components/ContactUs";
 import Home from "./components/Home";
-import Footer from "./components/Footer";
 import PriceLists from "./components/PriceLists";
 import PriceListConstruction from "./components/PriceListConstruction";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
+    <Router>
       <Nav />
-      <Home />
-      <PriceLists />
-      <PriceListConstruction />
-      <ContactUs />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/kontakt" element={<ContactUs />} />
+        <Route path="/prislista-tradfallning" element={<PriceLists />} />
+        <Route path="/prislista-bygg" element={<PriceListConstruction />} />
+      </Routes>
+    </Router>
   );
 }
 export default App;
